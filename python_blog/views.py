53bbd2a9d9bd5f8d01cd41a0625ad2ad1becbe3d
Korekiwi <foxiknyan@gmail.com>
2025-02-16 20:19:10 +0300

    поисковая форма, пагинатор

--- python_blog/views.py
+++ python_blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse
-from django.db.models import Count, F
+from django.db.models import Count, F, Q
+from django.core.paginator import Paginator
 
 from posts_dataset import dataset
 from help_functions import python_slugify, python_slugify_list
@@ -38,10 +39,53 @@ def about(request):
 
 
 def catalog_posts(request):
+    posts = (
+        Post.objects.select_related("category", "author")
+        .prefetch_related("tags")
+        .all()
+    )
+
+    search_query = request.GET.get("search_query", "")
+
+    if search_query:
+        q_object = Q()
+
+        if request.GET.get("search_text") == "1":
+            q_object |= Q(content__icontains=search_query)
+
+        if request.GET.get("search_title") == "1":
+            q_object |= Q(title__icontains=search_query)
+
+        if request.GET.get("search_tags") == "1":
+            q_object |= Q(tags__name__icontains=search_query)
+
+        if request.GET.get("search_category") == "1":
+            q_object |= Q(category__name__icontains=search_query)
+
+        if request.GET.get("search_slug") == "1":
+            q_object |= Q(slug__icontains=search_query)
+
+        if q_object:
+            posts = posts.filter(q_object).distinct()
+
+    sort_by = request.GET.get("sort_by", "created_date")
+
+    if sort_by == "views":
+        posts = posts.order_by("-views")
+    elif sort_by == "updated_date":
+        posts = posts.order_by("-updated_date")
+    elif sort_by == "created_date":
+        posts = posts.order_by("-published_date")
+
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get("page", 1)
+
+    page_obj = paginator.get_page(page_number)
 
     context = {
         "title": "Каталог постов",
-        "posts": Post.objects.select_related('category', 'author').prefetch_related('tags').all(),
+        "posts": page_obj,
     }
     
     return render(request, "catalog_posts.html", context)
